01d8bf3d95e52ab80545fa8005653df19951d1f7
Ahmed Sajjad <uasajjad11198@outlook.com>
2021-09-12 17:48:48 -0400

    started making timer

---
+++ assets/script/script.js
@@ -0,0 +1,21 @@
+var bodyEl = document.querySelector(".body");
+var highScoreEl = document.querySelector(".high-score");
+var timerDurationEl = document.querySelector(".timer-duration");
+var pageContentEl = document.querySelector("page-content");
+var mainContentEl = document.querySelector("main-content");
+var secondaryContentEl = document.querySelector("secondary-content");
+var tertiaryContentEl = document.querySelector("tertiary-content");
+var startBtnEl = document.querySelector(".start");
+var timeRemaining = 90
+var timeInterval = setInterval(function() {
+    if (timeRemaining > 1) {
+        timerDurationEl.textContent = timeRemaining;
+        timeRemaining--;
+    } else if (timeRemaining === 1) {
+        timerDurationEl.textContent = timeRemaining;
+        timeRemaining--;
+    } else {
+        timerDurationEl.textContent = '';
+        clearInterval(timeInterval)
+    }
+}, 1000);
